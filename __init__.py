326e09aa3358ed01411f2e9a611ef18fa27af9d3
csdjk <1076984375@qq.com>
2024-04-11 17:35:18 +0800

    update

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ import os
 import renderdoc
 import qrenderdoc as qrd
 import renderdoc as rd
-from typing import Optional
+from typing import List, Optional
 
 rd = renderdoc
 ResourceDescription = rd.ResourceDescription
@@ -63,7 +63,7 @@ def textureHasMipMap(tex: TextureDescription):
     return not (tex.mips == 1 and tex.msSamp <= 1)
 
 
-def SaveTexture(resourceId, controller, folderName):
+def SaveTexture(resourceId, controller, folderName, texName=""):
     texsave = rd.TextureSave()
 
     texsave.resourceId = resourceId
@@ -72,15 +72,27 @@ def SaveTexture(resourceId, controller, folderName):
 
     resourceDesc: rd.ResourceDescription = captureCtx.GetResource(resourceId)
     texture: rd.TextureDescription = captureCtx.GetTexture(resourceId)
+    # 小于4x4的纹理不导出,一般都是空白纹理
+    if texture.width <= 4 and texture.height <= 4:
+        return False
 
+    print(texture.format.Name())
+    print(resourceDesc.name)
     resourceIdStr = str(int(resourceId))
 
-    filename = f"{resourceDesc.name}_{resourceIdStr}"
+    # filename = f"{texName}_{resourceIdStr}"
+    filename = f"{texName}"
 
     texsave.mip = 0
     texsave.slice.depth = 0
     texsave.alpha = rd.AlphaMapping.Preserve
+
+    texFormat = ".tga"
     texsave.destType = rd.FileType.TGA
+    if texture.format.compType == rd.CompType.Float:
+        texsave.destType = rd.FileType.EXR
+        texFormat = ".exr"
+
     folderPath = f"{openDirectory}/{folderName}"
 
     if not os.path.exists(folderPath):
@@ -91,22 +103,45 @@ def SaveTexture(resourceId, controller, folderName):
             faces = ["X+", "X-", "Y+", "Y-", "Z+", "Z-"]
             for i in range(texture.arraysize):
                 texsave.slice.sliceIndex = i
-                outTexPath = f"{folderPath}/{filename}_{faces[i]}.tga"
+                outTexPath = f"{folderPath}/{filename}_{faces[i]}{texFormat}"
                 controller.SaveTexture(texsave, outTexPath)
         else:
             for i in range(texture.depth):
                 texsave.slice.sliceIndex = i
-                outTexPath = f"{folderPath}/{filename}_{i}.tga"
+                outTexPath = f"{folderPath}/{filename}_{i}{texFormat}"
                 controller.SaveTexture(texsave, outTexPath)
 
     else:
         texsave.slice.sliceIndex = 0
-        outTexPath = f"{folderPath}/{filename}.tga"
+        outTexPath = f"{folderPath}/{filename}{texFormat}"
         controller.SaveTexture(texsave, outTexPath)
 
     global textureCount
     textureCount += 1
     return True
+
+
+def BoundResourceName(state: rd.PipeState, bindPoint):
+
+    refl: rd.ShaderReflection = state.GetShaderReflection(rd.ShaderStage.Fragment)
+    mapping: rd.ShaderBindpointMapping = state.GetBindpointMapping(
+        rd.ShaderStage.Fragment
+    )
+    map_list: List[rd.Bindpoint] = mapping.readOnlyResources
+
+    try:
+        idx = map_list.index(bindPoint)
+    except:
+        print(f"not found bindPoint:{ bindPoint.bind}")
+        return ""
+
+    for res in refl.readOnlyResources:
+        res: rd.ShaderResource
+        if res.bindPoint == idx:
+            print(f"{res.name}: bindPoint:{ bindPoint.bind}")
+            return res.name
+
+    return ""
 
 
 # 导出当前Draw的所有Texture
@@ -115,11 +150,18 @@ def save_tex(controller: rd.ReplayController):
     textureCount = 0
 
     eventID = str(int(captureCtx.CurSelectedEvent()))
-    state = controller.GetPipelineState()
-    sampleList = state.GetReadOnlyResources(renderdoc.ShaderStage.Fragment)
+    state: rd.PipeState = controller.GetPipelineState()
+
+    # # 获取片元着色器的资源
+    sampleList: List[rd.BoundResourceArray] = state.GetReadOnlyResources(
+        renderdoc.ShaderStage.Fragment
+    )
     for sample in sampleList:
+        print(f"sample: { len(sample.resources)}-----------------------")
+        name = BoundResourceName(state, sample.bindPoint)
         for boundResource in sample.resources:
-            if not SaveTexture(boundResource.resourceId, controller, eventID):
+            boundResource: rd.BoundResource
+            if not SaveTexture(boundResource.resourceId, controller, eventID, name):
                 break
     captureCtx.Extensions().MessageDialog(
         f"Export Complete,Total {textureCount} textures:{openDirectory}",
@@ -143,6 +185,7 @@ def save_all_tex(controller: rd.ReplayController):
     global textureCount
     textureCount = 0
     for tex in captureCtx.GetTextures():
+        tex: rd.TextureDescription
         if not SaveTexture(tex.resourceId, controller, name):
             break
     captureCtx.Extensions().MessageDialog(
--- pythonShellDebug.py
+++ pythonShellDebug.py
@@ -3,7 +3,7 @@ import os
 import renderdoc
 import qrenderdoc as qrd
 import renderdoc as rd
-from typing import Optional
+from typing import List, Optional
 
 rd = renderdoc
 ResourceDescription = rd.ResourceDescription
@@ -40,7 +40,7 @@ def textureHasMipMap(tex: TextureDescription):
     return not (tex.mips == 1 and tex.msSamp <= 1)
 
 
-def SaveTexture(resourceId, controller, folderName):
+def SaveTexture(resourceId, controller, folderName, texName=""):
     texsave = rd.TextureSave()
 
     texsave.resourceId = resourceId
@@ -49,17 +49,29 @@ def SaveTexture(resourceId, controller, folderName):
 
     resourceDesc: rd.ResourceDescription = captureCtx.GetResource(resourceId)
     texture: rd.TextureDescription = captureCtx.GetTexture(resourceId)
+    # 小于4x4的纹理不导出,一般都是空白纹理
+    if texture.width <= 4 and texture.height <= 4:
+        return False
 
+    print(texture.format.Name())
+    print(resourceDesc.name)
     resourceIdStr = str(int(resourceId))
 
-    filename = f"{resourceDesc.name}_{resourceIdStr}"
+    # filename = f"{texName}_{resourceIdStr}"
+    filename = f"{texName}"
 
     texsave.mip = 0
     texsave.slice.depth = 0
     texsave.alpha = rd.AlphaMapping.Preserve
+
+    texFormat = ".tga"
     texsave.destType = rd.FileType.TGA
+    if texture.format.compType == rd.CompType.Float:
+        texsave.destType = rd.FileType.EXR
+        texFormat = ".exr"
+
     folderPath = f"{openDirectory}/{folderName}"
-    print(f"arraysize {texture.arraysize} depth {texture.depth}")
+
     if not os.path.exists(folderPath):
         os.makedirs(folderPath)
 
@@ -68,17 +80,17 @@ def SaveTexture(resourceId, controller, folderName):
             faces = ["X+", "X-", "Y+", "Y-", "Z+", "Z-"]
             for i in range(texture.arraysize):
                 texsave.slice.sliceIndex = i
-                outTexPath = f"{folderPath}/{filename}_{faces[i]}.tga"
+                outTexPath = f"{folderPath}/{filename}_{faces[i]}{texFormat}"
                 controller.SaveTexture(texsave, outTexPath)
         else:
             for i in range(texture.depth):
                 texsave.slice.sliceIndex = i
-                outTexPath = f"{folderPath}/{filename}_{i}.tga"
+                outTexPath = f"{folderPath}/{filename}_{i}{texFormat}"
                 controller.SaveTexture(texsave, outTexPath)
 
     else:
         texsave.slice.sliceIndex = 0
-        outTexPath = f"{folderPath}/{filename}.tga"
+        outTexPath = f"{folderPath}/{filename}{texFormat}"
         controller.SaveTexture(texsave, outTexPath)
 
     global textureCount
@@ -86,20 +98,51 @@ def SaveTexture(resourceId, controller, folderName):
     return True
 
 
+def BoundResourceName(state: rd.PipeState, bindPoint):
+
+    refl: rd.ShaderReflection = state.GetShaderReflection(rd.ShaderStage.Fragment)
+    mapping: rd.ShaderBindpointMapping = state.GetBindpointMapping(
+        rd.ShaderStage.Fragment
+    )
+    map_list: List[rd.Bindpoint] = mapping.readOnlyResources
+
+    try:
+        idx = map_list.index(bindPoint)
+    except:
+        print(f"not found bindPoint:{ bindPoint.bind}")
+        return ""
+
+    for res in refl.readOnlyResources:
+        res: rd.ShaderResource
+        if res.bindPoint == idx:
+            print(f"{res.name}: bindPoint:{ bindPoint.bind}")
+            return res.name
+
+    return ""
+
+
 # 导出当前Draw的所有Texture
-def save_tex(controller: rd.ReplayController):
+def save_tex(controller: rd.ReplayController):  # type: ignore
     global textureCount
     textureCount = 0
 
     eventID = str(int(captureCtx.CurSelectedEvent()))
-    state = controller.GetPipelineState()
-    sampleList = state.GetReadOnlyResources(renderdoc.ShaderStage.Fragment)
+    state: rd.PipeState = controller.GetPipelineState()
+
+    # # 获取片元着色器的资源
+    sampleList: List[rd.BoundResourceArray] = state.GetReadOnlyResources(
+        renderdoc.ShaderStage.Fragment
+    )
     for sample in sampleList:
+        print(f"sample: { len(sample.resources)}-----------------------")
+        name = BoundResourceName(state, sample.bindPoint)
         for boundResource in sample.resources:
-            if not SaveTexture(boundResource.resourceId, controller, eventID):
+            boundResource: rd.BoundResource  # type: ignore
+            if not SaveTexture(boundResource.resourceId, controller, eventID, name):
                 break
     captureCtx.Extensions().MessageDialog(
-        f"Export successful,A total of {textureCount}:{openDirectory}", "Export Texture"
+        f"Export Complete,Total {textureCount} textures:{openDirectory}",
+        "Export Texture",
     )
 
 
@@ -119,6 +162,7 @@ def save_all_tex(controller: rd.ReplayController):
     global textureCount
     textureCount = 0
     for tex in captureCtx.GetTextures():
+        tex: rd.TextureDescription
         if not SaveTexture(tex.resourceId, controller, name):
             break
     captureCtx.Extensions().MessageDialog(
